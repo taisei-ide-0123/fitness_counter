c6d7a3da3f1a18a5ed826ab568423d14ee3ca765
taisei0123 <taisei8955@gmail.com>
2021-04-12 21:13:06 +0900

    add materialize css

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -55,50 +55,83 @@ class Login extends Component {
     const errors = this.state.errors
 
     return (
-      <div>
-        <Link to="/">home</Link>
-        <div>
-          <Link to="/register">Register</Link>
+      <div className="container">
+        <div style={{ marginTop: '4rem' }} className="row">
+          <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+            <div className="col s12" style={{ paddingLeft: '11.250px' }}>
+              <h4>
+                <b>Sign In</b>
+              </h4>
+              <p className="grey-text text-darken-1">
+                Don't have an account?{' '}
+                <Link
+                  to="/register"
+                  style={{ color: '#ff5722', textDecoration: 'none' }}
+                >
+                  Register
+                </Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={this.onSubmit}>
+              <div className="input-field col s12">
+                <input
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"
+                  className={classnames('', {
+                    invalid: errors.email || errors.emailnotfound,
+                  })}
+                />
+                <label htmlFor="email">
+                  <i className="material-icons left">mail</i>Email
+                </label>
+                <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  className={classnames('', {
+                    invalid: errors.password || errors.passwordincorrect,
+                  })}
+                />
+                <label htmlFor="password">
+                  <i className="material-icons left">lock</i>Password
+                </label>
+                <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: '11.250px' }}>
+                <button
+                  style={{
+                    width: '150px',
+                    borderRadius: '3px',
+                    letterSpacing: '1.5px',
+                    marginTop: '1rem',
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable deep-orange"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <form noValidate onSubmit={this.onSubmit}>
-          <div>
-            <input
-              onChange={this.onChange}
-              value={this.state.email}
-              error={errors.email}
-              id="email"
-              type="email"
-              className={classnames('', {
-                invalid: errors.email || errors.emailnotfound,
-              })}
-            />
-            <label htmlFor="email">Email</label>
-            <span>
-              {errors.email}
-              {errors.emailnotfound}
-            </span>
-          </div>
-          <div>
-            <input
-              onChange={this.onChange}
-              value={this.state.password}
-              error={errors.password}
-              id="password"
-              type="password"
-              className={classnames('', {
-                invalid: errors.password || errors.passwordincorrect,
-              })}
-            />
-            <label htmlFor="password">Password</label>
-            <span>
-              {errors.password}
-              {errors.passwordincorrect}
-            </span>
-          </div>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
       </div>
     )
   }
