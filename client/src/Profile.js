70ecce84198c47483b5130495d1d3be158bba5a2
taisei0123 <taisei8955@gmail.com>
2021-04-19 00:24:16 +0900

    Description trainning menu

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import Nav from './components/Nav'
+import React, { Component } from 'react'
 import './App.css'
+import Nav from './components/Nav'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { logoutUser } from './actions/authActions'
 
-function Profile() {
-  return (
-    <div>
-      <Nav />
-      <h1>Profile page</h1>
-    </div>
-  )
+class Menu extends Component {
+  render() {
+    const { user } = this.props.auth
+
+    return (
+      <>
+        <Nav />
+        <div style={{ height: '75vh' }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <b>Hey there,</b> {user.name.split(' ')[0]} &nbsp;
+                {user.name.split(' ')[1]}
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into a full-stack{' '}
+                  <span style={{ fontFamily: 'monospace' }}>MERN</span> app 👏
+                </p>
+              </h4>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
-export default Profile
+Menu.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+})
+
+export default connect(mapStateToProps, { logoutUser })(Menu)
