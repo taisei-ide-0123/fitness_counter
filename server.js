b415d286eb6b9577f4952efcae9242b95b4bb44b
taisei0123 <taisei8955@gmail.com>
2021-04-30 19:42:24 +0900

    change server file

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 const port = process.env.PORT || 5000 // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`))
