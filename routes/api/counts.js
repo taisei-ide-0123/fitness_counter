68e04bd9388bf20b4d05f441e9e458235a83e15b
taisei0123 <taisei8955@gmail.com>
2021-05-02 13:41:14 +0900

    change file directory

--- routes/api/counts.js
+++ routes/api/counts.js
@@ -4,16 +4,16 @@ const router = express.Router()
 // Load Count model
 const Count = require('../../models/Count')
 
-router.route('/add').post((req, res) => {
+router.route('/:id').post((req, res) => {
   const user = req.params.user
   const event = req.body.event
   const count = Number(req.body.count)
   const date = Date.parse(req.body.date)
 
   const newCount = new count({
-    username,
-    description,
-    duration,
+    user,
+    event,
+    count,
     date,
   })
 
